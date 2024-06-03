ec914a610ac9a94524932adf5ee77059e7eadded
unknown <continuematical@zhou42538.outlook>
2024-06-03 11:01:28 +0800

    fix: :bug: fix bug

--- codelab-android-compose-main/AccessibilityCodelab/app/src/main/java/com/example/jetnews/JetnewsApplication.kt
+++ codelab-android-compose-main/AccessibilityCodelab/app/src/main/java/com/example/jetnews/JetnewsApplication.kt
@@ -20,6 +20,9 @@ import android.app.Application
 import com.example.jetnews.data.AppContainer
 import com.example.jetnews.data.AppContainerImpl
 
+/**
+ * JetnewsApplication
+ */
 class JetnewsApplication : Application() {
     
     lateinit var container: AppContainer
