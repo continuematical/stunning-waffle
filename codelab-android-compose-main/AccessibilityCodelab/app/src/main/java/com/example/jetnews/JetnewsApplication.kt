ecb95fa7f3b2628c6ed596380e74af32aee234f6
unknown <continuematical@zhou42538.outlook>
2024-06-03 10:59:26 +0800

    fix(:bug: fix bug): accessbility

--- codelab-android-compose-main/AccessibilityCodelab/app/src/main/java/com/example/jetnews/JetnewsApplication.kt
+++ codelab-android-compose-main/AccessibilityCodelab/app/src/main/java/com/example/jetnews/JetnewsApplication.kt
@@ -21,8 +21,7 @@ import com.example.jetnews.data.AppContainer
 import com.example.jetnews.data.AppContainerImpl
 
 class JetnewsApplication : Application() {
-
-    // AppContainer instance used by the rest of classes to obtain dependencies
+    
     lateinit var container: AppContainer
 
     override fun onCreate() {
